0cff6f5cac2f4faed192ce0305c96a893925b760
feki_mohamed <medfeki33@gmail.com>
2024-12-05 19:23:03 +0100

    corriger des erreurs

--- src/main/java/com/example/easy_notes/repository/NoteRepository.java
+++ src/main/java/com/example/easy_notes/repository/NoteRepository.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.easy_notes.note.Note;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
@@ -15,7 +13,5 @@ public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findByTitleStartingWith(String prefix);
     List<Note> findByTitleAndContent(String t, String c);
 
-    // Méthode corrigée pour compter les notes en fonction du contenu
-    @Query("SELECT COUNT(n) FROM Note n WHERE n.content LIKE %:content%")
-    int countByContent(@Param("content") String content);
+  
 }
