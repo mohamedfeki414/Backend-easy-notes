2386e0af146eb4996975c63a97e16a2dca3161a9
feki_mohamed <medfeki33@gmail.com>
2024-12-05 18:37:22 +0100

    modifier methode countByContent

--- src/main/java/com/example/easy_notes/repository/NoteRepository.java
+++ src/main/java/com/example/easy_notes/repository/NoteRepository.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.easy_notes.note.Note;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-	
-	List<Note>findByOrderByCreatedAtDesc();
-	List<Note>findByTitleStartingWith(String prefix);
-	List<Note>findByTitleAndContent(String t,String c);
-	int countByContentNbt(String content);
-	
-	}
+
+    List<Note> findByOrderByCreatedAtDesc();
+    List<Note> findByTitleStartingWith(String prefix);
+    List<Note> findByTitleAndContent(String t, String c);
+
+    // Méthode corrigée pour compter les notes en fonction du contenu
+    @Query("SELECT COUNT(n) FROM Note n WHERE n.content LIKE %:content%")
+    int countByContent(@Param("content") String content);
+}
